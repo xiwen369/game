92e15fef4f70e7e9dbc2203b6d778a47d06c24aa
xiwen369 <38783437+xiwen369@users.noreply.github.com>
2019-03-30 18:04:17 +0800

    分页实现代码

--- src/main/java/com/qiqiji/backstage/game/demo/demo2.java
+++ src/main/java/com/qiqiji/backstage/game/demo/demo2.java
@@ -1,6 +1,24 @@
 package com.qiqiji.backstage.game.demo;
-
+/**
+ * 1.计算总页数
+ * 2.实现分页-->上一页,下一页
+ * */
 public class demo2 {
-    //分页,运用三目运算符
+    public static void main(String[] args) {
+        //计算总页数
+        int totalPages;//总页数
+        int count = 99;//总条数
+        int pageSize = 10;//页大小
+        totalPages = count % pageSize > 0 ? count / pageSize + 1 : count /pageSize;
+        System.out.println("总页数=" + totalPages);
 
+        //实现分页
+        int previousPage;
+        int nextPage;
+        int currentPage = 9;
+        previousPage = currentPage > 1 ? currentPage - 1 : currentPage;
+        System.out.println("上一页=" + previousPage);
+        nextPage = currentPage < 10 ? currentPage + 1 : currentPage;
+        System.out.println("下一页=" + nextPage);
+    }
 }
