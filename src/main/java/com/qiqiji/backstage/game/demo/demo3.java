0ecf0267dcf3c9ff13cc3dde413a4180ef35f152
xiwen369 <38783437+xiwen369@users.noreply.github.com>
2019-04-02 09:22:54 +0800

    左移运算

---
+++ src/main/java/com/qiqiji/backstage/game/demo/demo3.java
@@ -0,0 +1,12 @@
+package com.qiqiji.backstage.game.demo;
+
+public class demo3 {
+    public static void main(String[] args) {
+        int a = 2;
+        int b;
+        b = a << 3;
+        System.out.println(b);
+
+    }
+
+}
