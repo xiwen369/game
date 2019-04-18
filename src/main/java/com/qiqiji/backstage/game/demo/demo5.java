e61577791e99308247a01b103bcdc54159534474
xiwen369 <38783437+xiwen369@users.noreply.github.com>
2019-04-18 15:53:57 +0800

    增强for循环操作数组

---
+++ src/main/java/com/qiqiji/backstage/game/demo/demo5.java
@@ -0,0 +1,19 @@
+package com.qiqiji.backstage.game.demo;
+
+public class demo5 {
+    /**
+     * java5新特性,可变参数
+     * */
+    public static void main(String[] args) {
+       double sum =getSum(1,2,3,4,5);
+        System.out.println(sum);
+    }
+
+    private static double getSum(double ... arr) {
+        double sum = 0.0;
+        for(double ele : arr){
+            sum += ele;
+        }
+        return sum;
+    }
+}
