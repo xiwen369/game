b0d099af1f6b2c71b936e985ffa981cb26d222d6
xiwen369 <38783437+xiwen369@users.noreply.github.com>
2019-04-19 13:42:48 +0800

    二分法查找

---
+++ src/main/java/com/qiqiji/backstage/game/demo/demo7.java
@@ -0,0 +1,50 @@
+package com.qiqiji.backstage.game.demo;
+
+public class demo7 {
+    /**
+     * 查找算法-二分法查找
+     */
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        banirySearch(arr, 7);
+    }
+
+    static int banirySearch(int[] arr, int key) {
+        int min = 0;
+        int max = arr.length-1;
+        int mid = 0;
+//        //第一次查找
+//        mid = (min + max) / 2;
+//        if (arr[mid] > key) {
+//            max = mid - 1;
+//        } else if (arr[mid] < key) {
+//            min = mid + 1;
+//        } else {
+//            return mid;
+//        }
+//        System.out.println(mid);
+//        //第二次查找
+//        mid = (min + max) / 2;
+//        if (arr[mid] > key) {
+//            max = mid - 1;
+//        } else if (arr[mid] < key) {
+//            min = mid + 1;
+//        } else {
+//            return mid;
+//        }
+//        System.out.println(mid);
+        //第N次查找
+        while (min<=max){
+            mid = (min + max) / 2;
+            if (arr[mid] > key) {
+                max = mid - 1;
+            } else if (arr[mid] < key) {
+                min = mid + 1;
+            } else {
+                System.out.println(mid);
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
