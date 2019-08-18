29d24be66e1662ad66fea2ff8861456f44ef03ac
YourName <38783437+xiwen369@users.noreply.github.com>
2019-08-18 15:35:54 +0800

    SpringBoot注解学习

---
+++ src/main/java/com/qiqiji/backstage/game/demo/Dog.java
@@ -0,0 +1,9 @@
+package com.qiqiji.backstage.game.demo;
+
+import lombok.Data;
+
+@Data
+public class Dog {
+    private String name;
+    private String age;
+}
---
+++ src/main/java/com/qiqiji/backstage/game/demo/Person1.java
@@ -0,0 +1,26 @@
+package com.qiqiji.backstage.game.demo;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.Map;
+
+/**
+ * 将配置文件中配置的每一个属性的值,映射到这个组件中
+ * @ConfigurationProperties: 告诉SpringBoot将本类中的所有属性和配置文件中相关的配置进行绑定
+ *      perfix = "person": 配置文件中哪个下面的所有属性进行一一映射
+ *只有这个组件是容器中的组件,才能提供@ConfigurationProperties功能,所有要加@Component注解
+ * */
+@Component
+@ConfigurationProperties(prefix = "person")
+@Data
+public class Person1 {
+    private String lastName;
+    private String age;
+    private Boolean boss;
+    private Date birth;
+    private Map<String,Object> maps;
+    private Dog dog;
+}
