3d390c8243b3ba23a252994b33fae77200f3953b
YourName <38783437+xiwen369@users.noreply.github.com>
2019-08-24 12:01:06 +0800

    SpringBoot注解学习

--- src/main/java/com/qiqiji/backstage/game/demo/Person1.java
+++ src/main/java/com/qiqiji/backstage/game/demo/Person1.java
@@ -1,7 +1,7 @@
 package com.qiqiji.backstage.game.demo;
 
 import lombok.Data;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -10,11 +10,13 @@ import java.util.Map;
 /**
  * 将配置文件中配置的每一个属性的值,映射到这个组件中
  * @ConfigurationProperties: 告诉SpringBoot将本类中的所有属性和配置文件中相关的配置进行绑定
- *      perfix = "person": 配置文件中哪个下面的所有属性进行一一映射
+ *      perfix = "person": 配置文件中哪个下面的所有属性进行一一映射 备注：默认从全局配置文件中获取
+ * @PropertySource:从指定的配置文件目录获取
  *只有这个组件是容器中的组件,才能提供@ConfigurationProperties功能,所有要加@Component注解
  * */
 @Component
-@ConfigurationProperties(prefix = "person")
+//@ConfigurationProperties(prefix = "person")
+@PropertySource(value={"classpath:person.yml"})
 @Data
 public class Person1 {
     private String lastName;
